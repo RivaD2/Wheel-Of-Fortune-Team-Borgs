a7a27728cb6380278dfad69b9d70f8629092964b
Riva Davidowski <v-rivad@microsoft.com>
2022-03-05 13:42:58 -0800

    adding all files from project

---
+++ LeapWoF/GameManager.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using LeapWoF.Interfaces;
+
+namespace LeapWoF
+{
+
+    /// <summary>
+    /// The GameManager class, handles all game logic
+    /// </summary>
+    public class GameManager
+    {
+
+        /// <summary>
+        /// The input provider
+        /// </summary>
+        private IInputProvider inputProvider;
+
+        /// <summary>
+        /// The output provider
+        /// </summary>
+        private IOutputProvider outputProvider;
+
+        private string TemporaryPuzzle;
+        public List<string> charGuessList = new List<string>();
+
+        public GameState GameState { get; private set; }
+        // Don't change this constructor, intiate a new Game Player
+        public GameManager() : this(new ConsoleInputProvider(), new ConsoleOutputProvider())
+        {
+
+        }
+
+        public GameManager(IInputProvider inputProvider, IOutputProvider outputProvider)
+        {
+            if (inputProvider == null)
+                throw new ArgumentNullException(nameof(inputProvider));
+            if (outputProvider == null)
+                throw new ArgumentNullException(nameof(outputProvider));
+
+            this.inputProvider = inputProvider;
+            this.outputProvider = outputProvider;
+
+            GameState = GameState.WaitingToStart;
+        }
+
+        /// <summary>
+        /// Manage game according to game state
+        /// </summary>
+        public void StartGame()
+        {
+            InitGame();
+
+            while (true)
+            {
+
+                PerformSingleTurn();
+
+                if (GameState == GameState.RoundOver)
+                {
+                    CheckPlayerSolution();
+                    StartNewRound();
+                    continue;
+                }
+
+                if (GameState == GameState.GameOver)
+                {
+                    outputProvider.WriteLine("Game over");
+                    break;
+                }
+            }
+        }
+        public void StartNewRound()
+        {
+            TemporaryPuzzle = "Hello World";
+            // update the game state
+            GameState = GameState.RoundStarted;
+        }
+
+        public string CheckPlayerSolution()
+        {
+            foreach(var charItem in charGuessList)
+            {
+                if(TemporaryPuzzle.Contains(charItem))
+                {
+                   outputProvider.WriteLine($"You guessed: {charItem}");
+                } 
+            }
+            return TemporaryPuzzle;
+        }
+    
+      
+        public void PerformSingleTurn()
+        {
+            //outputProvider.Clear();
+            DrawPuzzle();
+            outputProvider.WriteLine("Type 1 to spin, 2 to solve");
+            GameState = GameState.WaitingForUserInput;
+
+            var action = inputProvider.Read();
+
+            switch (action)
+            {
+                case "1":
+                    Spin();
+                    break;
+                case "2":
+                    Solve();
+                    break;
+            }
+
+        }
+
+        /// <summary>
+        /// Draw the puzzle
+        /// </summary>
+        private void DrawPuzzle()
+        {
+            outputProvider.WriteLine("The puzzle is:");
+            outputProvider.WriteLine(TemporaryPuzzle);
+            outputProvider.WriteLine();
+        }
+
+        /// <summary>
+        /// Spin the wheel and do the appropriate action
+        /// </summary>
+        public void Spin()
+        {
+            outputProvider.WriteLine("Spinning the wheel...");
+            //TODO - Implement wheel + possible wheel spin outcomes
+           
+            GuessLetter();
+        }
+
+        public void Solve()
+        {
+            outputProvider.Write("Please enter your solution:");
+            var guess = inputProvider.Read();
+            if (guess.ToLower() == TemporaryPuzzle.ToLower())
+            {
+                GameState = GameState.GameOver;
+                outputProvider.WriteLine("Congrats! You solved the puzzle!");
+            }
+        }
+        public void GuessLetter()
+        {
+            outputProvider.Write("Please guess a letter: ");
+            var guess = inputProvider.Read();
+            charGuessList.Add(guess);
+            GameState = GameState.RoundOver;
+        }
+
+        /// <summary>
+        /// Optional logic to accept configuration options
+        /// </summary>
+        public void InitGame()
+        {
+
+            outputProvider.WriteLine("Welcome to Wheel of Fortune!");
+            StartNewRound();
+        }
+    }
+}
---
+++ LeapWoF/GameState.cs
@@ -0,0 +1,15 @@
+﻿
+namespace LeapWoF
+{
+    public enum GameState
+    {
+        // Don't make changes here
+        WaitingToStart,
+        RoundStarted,
+        RoundOver,
+        WaitingForUserInput,
+        GuessingLetter,
+        Solving,
+        GameOver
+    }
+}
